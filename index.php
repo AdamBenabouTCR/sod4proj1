006f165520447e79804b28e21ddfd87f706b06d5
AdamBenabouTCR <94106062+AdamBenabouTCR@users.noreply.github.com>
2023-10-22 17:26:59 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Index</title>
-    <link rel="stylesheet" href="styles/indexstyle.css">
+    <link rel="stylesheet" href="styles/mainstyle.css">
 </head>
 <body>
 
